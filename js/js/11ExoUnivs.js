a1b9f099e23970c44221aafb6c9bd18f9a2fa30c
yvandouenel <y.douenel@coopernet.fr>
2023-03-02 14:26:09 +0100

    Ennoncé exo univ.

--- js/js/11ExoUnivs.js
+++ js/js/11ExoUnivs.js
@@ -1,12 +1,14 @@
-fetch("https://www.cooooopernet.fr/session/token")
-  .then(response => {
-    console.log(`statut de la réponse`, response.status);
-    if(response.status != 200) return new Error("Pb de statut de la réponse");
-    return response.text();
-  })
-  .then(token => {
-    console.log(`token : `, token);
-  })
-  .catch(error => {
-    console.error(`Erreur attrapée`, error);
-  });
+//Gestion de l'évément submit sur le formulaire
+// récupération d'une référence vers le formulaire et assignation à la const "form"
+
+// Assignation de la propriété onsubmit sur le formulaire. ATTENTION à supprimer le comporterment par défaut qui recharge la page (e.preventDefault())
+
+// Récupération de la "value" du select
+
+// Appel de fetch avec la bonne url (concaténation utilisant la value du select)
+
+// Utilisation des then et des catch ou des async await
+
+// Déclaration et assignation de la variable "univs" avec le résultat (cas favorable) du fetch
+
+// parcours du tableau "univs" et creation des instances de Univ (classe) avec appel de la méthode "render" qui gère le dom (création de balises section h2 a) en utilisant la class bootstrap col-3
