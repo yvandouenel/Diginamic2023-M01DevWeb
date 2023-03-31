3e4e33b01a28e8f1e560a15abd4b76ace732f40a
yvandouenel <y.douenel@coopernet.fr>
2023-03-31 16:29:05 +0200

    tests sur les fichiers.

--- php/testFiles/index.php
+++ php/testFiles/index.php
@@ -1,8 +1,29 @@
 <?php
-$f = "myfile.txt";
+/* $f = "myfile.txt";
     
 $content = "The quick brown fox jumps over the lazy dog."; // String of data to be written
 $file = fopen($f, "w") or die("ERROR: Cannot open the file."); // Open the file for writing
 fwrite($file, $content) or die ("ERROR: Cannot write to file."); // Write data to the file
 fclose($file); // Closing the file
-echo "Data written successfully.";
+echo "Data written successfully."; */
+
+/* Test de lecture d'un fichier csv */
+$filename = "plats.csv";
+
+/* création d'un tableau */
+$categories = [];
+// Check the existence of file
+if (file_exists($filename)) {
+    // Reading the entire file into an array
+    $content = file($filename) or die("ERROR: Cannot open the file.");
+    // Parcours du fichier ligne par ligne
+    foreach($content as $line){
+      $plat = explode(";", $line);
+      // Ajout d'un élément de tableau à la valeur qui correspond à la clé de la catégorie de plat
+      $categories[$plat[0]][] = $plat[1];
+
+    }
+} else {
+    echo "ERROR: File does not exist.";
+}
+var_dump($categories);
