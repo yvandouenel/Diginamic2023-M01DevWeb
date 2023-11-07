2566500dd07a87356e507c60160d68870bde7ec1
yvandouenel <y.douenel@coopernet.fr>
2023-11-07 16:17:46 +0100

    photo gallery en cours.

---
+++ ionic/photo-gallery/src/pages/Tab2.tsx
@@ -0,0 +1,52 @@
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+import { useState } from 'react';
+import { UserPhoto } from '../hooks/usePhotoGallery';
+
+const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
